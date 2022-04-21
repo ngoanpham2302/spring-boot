bff47b2c8db56a7aa186a1fbf95f95631ce273b7
ngoanpham2302 <ngoanptp2302@gmail.com>
2022-04-21 20:22:47 +0700

    update RandomController.java

--- 01/basicweb/src/main/java/vn/techmaster/basicweb/controller/RandomController.java
+++ 01/basicweb/src/main/java/vn/techmaster/basicweb/controller/RandomController.java
@@ -9,23 +9,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/random")
 public class RandomController {
-    private static final String alpha = "abcdefghijklmnopqrstuvwxyz";
-    private static final String alphaUpperCase = alpha.toUpperCase();
-    private static final String digits = "0123456789";
-    private static final String ALPHA_NUMERIC = alpha + alphaUpperCase + digits;
-    private static final int strLength = 8;
-
-    // Trả về một chuỗi random string gồm 8 ký tự A-Z, a-z, 0-9
+    // ------ Cách 1: Dùng IntStream
     @GetMapping
     public String getRandomString() {
-        StringBuilder sb = new StringBuilder();
+        int strLength = 8;
+        int leftLimit = 48;
+        int rightLimit = 122;
         Random random = new Random();
+        // 48 -> 57: '0-9'; 65 -> 90: 'A-Z'; 97 -> 122: 'a-z'
 
-        for (int i = 0; i < strLength; i++) {
-            int randomNum = random.nextInt(ALPHA_NUMERIC.length());
-            sb.append(ALPHA_NUMERIC.charAt(randomNum));
-        }
+        String randomStr = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || (i >= 65 && i <= 90) || i >= 97))
+                .limit(strLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
 
-        return sb.toString();
+        return randomStr;
     }
+
+    // ------ Cách 2:
+    // private static final String alpha = "abcdefghijklmnopqrstuvwxyz";
+    // private static final String alphaUpperCase = alpha.toUpperCase();
+    // private static final String digits = "0123456789";
+    // private static final String ALPHA_NUMERIC = alpha + alphaUpperCase + digits;
+    // private static final int strLength = 8;
+
+    // // Trả về một chuỗi random string gồm 8 ký tự A-Z, a-z, 0-9
+    // @GetMapping
+    // public String getRandomString() {
+    // StringBuilder sb = new StringBuilder();
+    // Random random = new Random();
+
+    // for (int i = 0; i < strLength; i++) {
+    // int randomNum = random.nextInt(ALPHA_NUMERIC.length());
+    // sb.append(ALPHA_NUMERIC.charAt(randomNum));
+    // }
+
+    // return sb.toString();
+    // }
 }
