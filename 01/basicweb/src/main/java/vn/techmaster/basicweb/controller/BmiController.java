57fe7bd1e3785ae391ae5701d1e6269115882879
ngoanpham2302 <ngoanptp2302@gmail.com>
2022-04-22 10:35:53 +0700

    update book-rest

--- 01/basicweb/src/main/java/vn/techmaster/basicweb/controller/BmiController.java
+++ 01/basicweb/src/main/java/vn/techmaster/basicweb/controller/BmiController.java
@@ -1,20 +1,16 @@
 package vn.techmaster.basicweb.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import vn.techmaster.basicweb.model.Bmi;
 
 @RestController
 @RequestMapping("/bmi")
 public class BmiController {
 
     @PostMapping
-    public double calculateBmi(@RequestBody Bmi bodyInfo) {
-        double height = bodyInfo.getHeight();
-        double weight = bodyInfo.getWeight();
+    public double calculateBmi(@RequestParam("h") double height, @RequestParam("w") double weight) {
         double bmiIndex = weight / (height * height);
         return (Math.round(bmiIndex * 10.0)) / 10.0;
     }
--- 01/book-rest/src/main/java/vn/techmaster/bookrest/controller/BookController.java
+++ 01/book-rest/src/main/java/vn/techmaster/bookrest/controller/BookController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import vn.techmaster.bookrest.dto.BookRequest;
 import vn.techmaster.bookrest.model.Book;
@@ -36,22 +39,30 @@ public class BookController {
 
     // Add new book
     @PostMapping
-    public Book addNewBook(@RequestBody BookRequest bookRequest) {
+    public ResponseEntity<Book> addNewBook(@RequestBody BookRequest bookRequest) {
         String uuid = UUID.randomUUID().toString();
         Book newBook = new Book(uuid, bookRequest.title(), bookRequest.author(), bookRequest.year());
         books.put(uuid, newBook);
-        return newBook;
+        return ResponseEntity.status(HttpStatus.CREATED).body(newBook);
     }
 
     // Get book by id
     @GetMapping("/{id}")
     public Book getBookById(@PathVariable("id") String id) {
+        Book book = books.get(id);
+        if (book == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Book Not Found");
+        }
         return books.get(id);
     }
 
     // Update book by id
     @PutMapping("/{id}")
     public Book updateBookById(@PathVariable("id") String id, @RequestBody BookRequest bookRequest) {
+        Book book = books.get(id);
+        if (book == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Book Not Found");
+        }
         Book updatedBook = new Book(id, bookRequest.title(), bookRequest.author(), bookRequest.year());
         books.put(id, updatedBook);
         return updatedBook;
@@ -60,6 +71,10 @@ public class BookController {
     // Delete book by id
     @DeleteMapping("/{id}")
     public Book deleteBookById(@PathVariable("id") String id) {
+        Book book = books.get(id);
+        if (book == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Book Not Found");
+        }
         Book removedBook = books.remove(id);
         return removedBook;
     }
