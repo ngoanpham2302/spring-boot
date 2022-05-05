ccdd5aceeab08b4962c53cfc68e12588405afe35
ngoanpham2302 <ngoanptp2302@gmail.com>
2022-05-05 16:08:37 +0700

    add comment in App.java

--- 06/java-reflect-di/src/main/java/App.java
+++ 06/java-reflect-di/src/main/java/App.java
@@ -1,10 +1,16 @@
+import components.Emailer;
+import foo.Bar;
+
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        demoComponentScan();
         inspectFooBar();
+        System.out.println("---------------");
+        demoComponentScan();
     }
 
     private static void inspectFooBar() {
@@ -12,17 +18,31 @@ public class App {
         try {
             Object obj = customClassLoader.findClass("foo.Bar").getDeclaredConstructor().newInstance();
             Class<?> objClass = obj.getClass();
-            Method[] methods = objClass.getDeclaredMethods();
-            Class<?>[] interfaces = objClass.getInterfaces();
-            System.out.println(String.format("Method of %s class", obj.getClass().getName()));
+            // Class<?> objClass = Class.forName("foo.Bar");   // C2: tạo ra đối tượng kiểu Class
+            // Bar obj = (Bar) objClass.getDeclaredConstructor().newInstance();
 
-            for (Method method: methods) {
+            Method[] methods = objClass.getDeclaredMethods(); // Lấy ra danh sách các method của class
+            Class<?>[] interfaces = objClass.getInterfaces();
+
+            System.out.println(String.format("\nMethod of %s class: ", objClass.getName()));
+            for (Method method : methods) {
                 System.out.println(method.getName());
-                method.invoke(obj, null);
+                method.invoke(obj, null);   // Thực thi method của đối tượng obj với các tham số args
+                System.out.println();
             }
 
+            System.out.println("Interfaces of class " + objClass.getName() + ":");
             for (Class<?> i : interfaces) {
                 System.out.println(i.getName());
+            }
+
+            // Lấy annotations:
+            // Class<?> aClazz = Class.forName("components.Emailer");
+            Class<Emailer> aClazz = Emailer.class;
+            Annotation[] annotations = aClazz.getAnnotations();
+            System.out.println("\nAnnotations of class " + aClazz.getSimpleName() + ":");
+            for (Annotation a : annotations) {
+                System.out.println(a.annotationType());
             }
 
         } catch (Exception e) {
@@ -31,6 +51,7 @@ public class App {
     }
 
     private static void demoComponentScan() {
+        System.out.println("@Component: ");
         ClasspathInspector.componentScan().forEach(e -> System.out.println(e.getName()));
     }
 }
--- 06/java-reflect-di/src/main/java/ClasspathInspector.java
+++ 06/java-reflect-di/src/main/java/ClasspathInspector.java
@@ -15,13 +15,15 @@ import java.util.stream.Collectors;
 
 public class ClasspathInspector {
     static boolean DEBUG = false;
-    public static List<Class<?>> getAllKnownClasses () {
+
+    public static List<Class<?>> getAllKnownClasses() {
         List<Class<?>> classFiles = new ArrayList<>();
         List<File> classLocations = getClassLocationsForCurrentClasspath();
-//        classLocations.stream().map(file -> getClassesFromPath(file)).collect(Collectors.toList());
-        for (File file: classLocations) {
+        // classLocations.stream().map(file -> getClassesFromPath(file)).collect(Collectors.toList());
+        for (File file : classLocations) {
             classFiles.addAll(getClassesFromPath(file));
         }
+
         return classFiles;
     }
 
@@ -29,7 +31,7 @@ public class ClasspathInspector {
         List<File> urls = new ArrayList<>();
         String javaClasspath = System.getProperty("java.class.path");
         if (javaClasspath != null) {
-            for (String path: javaClasspath.split(File.pathSeparator)) {
+            for (String path : javaClasspath.split(File.pathSeparator)) {
                 urls.add(new File(path));
             }
         }
@@ -61,7 +63,6 @@ public class ClasspathInspector {
                     }
                 }
             }
-
         } catch (Exception e) {
             throw new RuntimeException("Fail to read classes from JAR file " + path, e);
         }
@@ -70,51 +71,56 @@ public class ClasspathInspector {
 
     private static String fromFileToClassname(String fileName) {
         return fileName.substring(0, fileName.length() - 6).replaceAll("/|\\\\", "\\.");
+        // return fileName.substring(0, fileName.length() - 6).replaceAll("[/\\\\]", "\\.");
     }
 
     private static Collection<? extends Class<?>> getClassesFromDirectory(File path) {
         List<Class<?>> classes = new ArrayList<>();
+        // DEBUG = true;
         log("\t getClassesFromDirectory : get classes for " + path);
 
         // Get jar files from top-level directory
-        List<File> jarFiles = listFiles(path, new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".jar");
-            }
-        }, false);
+        List<File> jarFiles = listFiles(path, (dir, name) -> name.endsWith(".jar"), false);
+        /*
+         List<File> jarFiles = listFiles(path, new FilenameFilter() {
+             @Override
+             public boolean accept(File dir, String name) {
+                 return name.endsWith(".jar");
+             }
+         }, false); */
 
-        for (File file: jarFiles) {
+        for (File file : jarFiles) {
             classes.addAll(getClassesFromJarFile(path));
         }
 
         // get all class-files
-        List<File> classFiles = listFiles(path, new FilenameFilter() {
+        List<File> classFiles = listFiles(path, (dir, name) -> name.endsWith(".class"), true);
+        /* List<File> classFiles = listFiles(path, new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
                 return name.endsWith(".class");
             }
-        }, true);
+        }, true); */
 
         int subStringBeginIndex = path.getAbsolutePath().length() + 1;
 
-        for (File classFile:classFiles) {
+        for (File classFile : classFiles) {
             String className = fromFileToClassname(classFile.getAbsolutePath().substring(subStringBeginIndex));
             log("Found class %s in path %s", className, path);
             try {
                 classes.add(Class.forName(className));
-            } catch(Throwable e) {
+            } catch (Throwable e) {
                 log("Could not create class %s. %s", className, e);
             }
         }
-        
+
         return classes;
     }
 
     private static List<File> listFiles(File directory, FilenameFilter filenameFilter, boolean recursive) {
         List<File> files = new ArrayList<>();
         File[] entries = directory.listFiles();
-        for (File entry: entries) {
+        for (File entry : entries) {
             // no filter or the filter accepts the file/directory, add it to the list
             if (filenameFilter == null || filenameFilter.accept(directory, entry.getName())) {
                 files.add(entry);
@@ -151,7 +157,9 @@ public class ClasspathInspector {
     public static void main(String[] args) {
         // find all classes in classpath
         List<Class<?>> allClasses = ClasspathInspector.getAllKnownClasses();
-        System.out.printf("There are %s class available in the classpath\n", allClasses.size());
+        System.out.printf("There are %s class available in the classpath:\n", allClasses.size());
+        allClasses.forEach(claz -> System.out.println(claz.getName()));
+        System.out.println();
 
         // find all classes that implement/subclass an interface/superclass
         // List<Class<?>> serializableClasses = ClasspathInspector.getMatchingClasses(Serializable.class);
