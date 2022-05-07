f75ee82b49468b8790fce329e62cc06f3e1c1910
ngoanpham2302 <ngoanptp2302@gmail.com>
2022-05-08 02:05:24 +0700

    update EmployerRepository

--- 08/jobhunt/src/main/java/vn/techmaster/jobhunt/controller/EmployerController.java
+++ 08/jobhunt/src/main/java/vn/techmaster/jobhunt/controller/EmployerController.java
@@ -1,11 +1,9 @@
 package vn.techmaster.jobhunt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +11,7 @@ import vn.techmaster.jobhunt.model.Employer;
 import vn.techmaster.jobhunt.repository.EmployerRepository;
 import vn.techmaster.jobhunt.request.EmployerRequest;
 
-import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -23,7 +21,8 @@ public class EmployerController {
 
     @GetMapping("/employers")
     public String getEmployerList(Model model) {
-        model.addAttribute("employers", employerRepo.getEmployerList());
+        List<Employer> employerList = employerRepo.getEmployers().values().stream().toList();
+        model.addAttribute("employers", employerList);
         return "employers";
     }
 
@@ -45,7 +44,7 @@ public class EmployerController {
             employer.setPhone(empRequest.getPhone());
             employer.setLocation(empRequest.getLocation());
 
-            employerRepo.getEmployerList().add(employer);
+            employerRepo.getEmployers().put(uuid, employer);
         }
         return "redirect:/employers";
     }
--- 08/jobhunt/src/main/java/vn/techmaster/jobhunt/repository/EmployerRepository.java
+++ 08/jobhunt/src/main/java/vn/techmaster/jobhunt/repository/EmployerRepository.java
@@ -5,22 +5,20 @@ import lombok.Setter;
 import org.springframework.stereotype.Repository;
 import vn.techmaster.jobhunt.model.Employer;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 @Getter
 @Setter
 public class EmployerRepository {
-    private List<Employer> employerList;
+    private ConcurrentHashMap<String, Employer> employers;
 
     public EmployerRepository() {
-        employerList = new ArrayList<>(List.of(
-                new Employer("EMP1", "Samsung", "ss@gmail.com", "0357456789","Hà Nội"),
-                new Employer("EMP2", "Viettel", "vt@gmail.com", "0336578923","Hải Phòng"),
-                new Employer("EMP3", "FPT", "fpt@gmail.com", "0987765234","Bắc Ninh"),
-                new Employer("EMP4", "Vinamilk", "vnm@gmail.com", "0991204569","Bắc Ninh"),
-                new Employer("EMP5", "VNPT", "vnpt@gmail.com", "088576495","Hà Nội")
-        ));
+        employers = new ConcurrentHashMap<>();
+        employers.put("EMP1", new Employer("EMP1", "Samsung", "ss@gmail.com", "0357456789","Hà Nội"));
+        employers.put("EMP2", new Employer("EMP1", "Samsung", "ss@gmail.com", "0357456789","Hà Nội"));
+        employers.put("EMP3",new Employer("EMP3", "FPT", "fpt@gmail.com", "0987765234","Bắc Ninh"));
+        employers.put("EMP4", new Employer("EMP4", "Vinamilk", "vnm@gmail.com", "0991204569","Bắc Ninh"));
+        employers.put("EMP5", new Employer("EMP5", "VNPT", "vnpt@gmail.com", "0885764952","Hà Nội"));
     }
 }
