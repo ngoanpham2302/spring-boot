ce00c80050b1e047ade4b5086ae014c73fdf99f0
ngoanpham2302 <ngoanptp2302@gmail.com>
2022-06-02 23:37:12 +0700

    modified ReadMe.MD

--- exam/springjpa/src/main/java/vn/techmaster/springjpa/repository/CourseRepository.java
+++ exam/springjpa/src/main/java/vn/techmaster/springjpa/repository/CourseRepository.java
@@ -1,16 +1,12 @@
 package vn.techmaster.springjpa.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import vn.techmaster.springjpa.entity.Course;
 
 public interface CourseRepository extends JpaRepository {
 
     // Viết Native Query để tính điểm trung bình một môn bất kỳ
 
-    @Query(value = "select * from student  where first_name = ?1 ", nativeQuery = true)
-    List<Course> calAverageScoreByCourse();
+    @Query(value = "select avg(score) from course c inner join student_course sc on c.id = sc.course_id where c.name = ?1 group by c.id", nativeQuery = true)
+    float calAverageScoreByCourse(String courseName);
 }
--- exam/springjpa/src/main/java/vn/techmaster/springjpa/repository/StudentRepository.java
+++ exam/springjpa/src/main/java/vn/techmaster/springjpa/repository/StudentRepository.java
@@ -15,7 +15,7 @@ public interface StudentRepository extends JpaRepository {
     Map<String, List<Student>> getStudentByCourse();
 
     // Liệt kê danh sách sinh viên học math nhưng không học music
-    @Query(value = "select * from student  where first_name = ?1 ", nativeQuery = true)
+    @Query(value = "select * from student s inner join student_course sc on s.id = sc.student_id inner join course c on c.id = sc.course_id where c.name = 'math' and c.name != 'music'", nativeQuery = true)
     List<Student> searchByCourse();
 
 }
